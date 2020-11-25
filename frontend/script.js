50e63a7d586ee259cc2be7074d794265783ac84c
Patrick Schneeberger <patrick_schneeberge@sluz.ch>
2020-11-25 11:30:14 +0100

    frontent update

---
+++ frontend/script.js
@@ -0,0 +1,10 @@
+//deno run --allow-net --allow-read .\webserver.js
+
+import { Application, Router, send } from "https://deno.land/x/oak@v6.3.1/mod.ts";
+const app = new Application();
+const router = new Router();
+router
+    .get("", (context) => { return send(context, "index.html") });
+    
+app.use(router.routes());
+app.listen({port:8000});
